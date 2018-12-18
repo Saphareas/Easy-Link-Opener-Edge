fdb11884d1b7352b1fdcc8d7676511298a829a6a
Fabian Große <saphareas@googlemail.com>
2018-12-18 20:44:28 +0100

    Add License information to files

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2018 Fabian Große
+ * This software is licensed under the GNU General Public License 3 (GPLv3) or later.
+ * For details please see the LICENSE file that should be included with this software.
+ */
+
 function onCreated() {
     return
 }
